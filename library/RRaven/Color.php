7ff0250bbd83b571687bb102d572dc2479ac77b4
David Godfrey <reactiveraven@gmail.com>
2012-02-06 11:43:08 +0000

    Adding comments and 'fromHSL' to RRaven_Color

--- library/RRaven/Color.php
+++ library/RRaven/Color.php
@@ -6,8 +6,16 @@ abstract class RRaven_Color {
 	protected $_rgb = null;
 	protected $_hex = null;
 	
+	/**
+	 * Protected to require using the 'from' methods
+	**/
 	protected function __construct();
 	
+	/**
+	 * Returns a RRaven_Color object based on the given RGB array
+	 *
+	 * @param $rgb int[] red, green, blue values in range 0-255
+	**/
 	public static function fromRGB($rgb)
 	{
 		$color = new RRaven_Color();
@@ -15,6 +23,11 @@ abstract class RRaven_Color {
 		return $color;
 	}
 	
+	/**
+	 * Returns a RRaven_Color object based on the given Hex string
+	 * 
+	 * @param $hex string
+	**/
 	public static function fromHex($hex)
 	{
 		$color = new RRaven_Color();
@@ -22,6 +35,23 @@ abstract class RRaven_Color {
 		return $color;
 	}
 	
+	/**
+	 * Returns a RRaven_Color object based on the given HSL array
+	 *
+	 * @param $hsl float[] hue, saturation, luminence values in ranges 0-360, 0-1, 0-1
+	**/
+	public static function fromHSL($hsl)
+	{
+		$color = new RRaven_Color();
+		$color->_hsl = $hsl;
+		return $color;
+	}
+	
+	/**
+	 * Returns the RGB array of this color.
+	 *
+	 * @return int[] red, green, blue in range 0-255
+	**/
 	protected function getRGB()
 	{
 		if (is_array($this->_rgb)) {
@@ -30,39 +60,74 @@ abstract class RRaven_Color {
 		return $this->_rgb = RRaven_Convert_Color::hsl_to_rgb($this->_hsl);
 	}
 	
+	/**
+	 * Returns hue degree for this color
+	 *
+	 * @return int hue in range 0-360
+	**/
 	public function getHue()
 	{
 		return $this->_hsl[0];
 	}
 	
+	/**
+	 * Returns saturation for this color
+	 * 
+	 * @return float saturation in range 0-1
+	**/
 	public function getSaturation()
 	{
 		return $this->_hsl[1];
 	}
 	
-	public function getBrightness()
+	/**
+	 * Returns lightness for this color
+	 *
+	 * @return float lightness in range 0-1
+	**/
+	public function getLightness()
 	{
 		return $this->_hsl[2];
 	}
 	
+	/**
+	 * Returns red value for this color
+	 *
+	 * @return int red in range 0-255
+	**/
 	public function getRed()
 	{
 		$rgb = $this->getRGB();
 		return $rgb[0];
 	}
 	
+	/**
+	 * Returns green value for this color
+	 *
+	 * @return int green in range 0-255
+	**/
 	public function getGreen()
 	{
 		$rgb = $this->getRGB();
 		return $rgb[1];
 	}
 	
+	/**
+	 * Returns blue value for this color
+	 *
+	 * @return int blue in range 0-255
+	**/
 	public function getBlue()
 	{
 		$rgb = $this->getRGB();
 		return $rgb[2];
 	}
 	
+	/**
+	 * Returns hex string value for this color
+	 *
+	 * @return string
+	**/
 	public function getHex()
 	{
 		return 
